8a51bbb2ff914a70b7c36d2f8fa7ccc5e0e46095
Colin Suckow <csuckow99@gmail.com>
2018-11-14 10:12:49 -0800

    Added test stations

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function updateView() {
   
   map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/dark-v9',
+      style: 'mapbox://styles/mapbox/light-v9',
       center: [-121.321807, 44.056626],
       zoom: 13
   });
@@ -28,8 +28,8 @@ function mapTest(data) {
   var end_lat = data.end_lat;
   var end_lon = data.end_lon;
 
-  var requestUrl = `https://api.mapbox.com/directions/v5/mapbox/driving/${start_lon}%2C${start_lat}%3B${end_lon}%2C${end_lat}.json?access_token=${MAPBOX_KEY}&geometries=geojson`
-  console.log(request);
+  var requestUrl = `https://api.mapbox.com/directions/v5/mapbox/cycling/${start_lon}%2C${start_lat}%3B${end_lon}%2C${end_lat}.json?access_token=${MAPBOX_KEY}&geometries=geojson`
+  console.log(requestUrl);
   var mapLine = $.getJSON(requestUrl, loadMapData)
 }
 
@@ -39,6 +39,7 @@ function loadMapData(data) {
 
   console.log(routeData);
   
+  //Add test route line
   map.addLayer({
     "id": "route",
     "type": "line",
@@ -55,4 +56,105 @@ function loadMapData(data) {
       "line-width": 4
   }
   });
+
+  //Add station points
+  map.addLayer({
+    "id": "points",
+    "type": "symbol",
+    "source": {
+        "type": "geojson",
+        "data": {
+            "type": "FeatureCollection",
+            "features": [{
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [-121.32598,  44.048745]
+                },
+                "properties": {
+                    "title": "Columbia Simpson",
+                    "icon": "monument"
+                }
+            }, {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [-121.31667, 44.058705]
+                },
+                "properties": {
+                    "title": "Drake Park",
+                    "icon": "monument"
+                }
+            }, {
+              "type": "Feature",
+              "geometry": {
+                  "type": "Point",
+                  "coordinates": [-121.31425, 44.05733]
+              },
+              "properties": {
+                  "title": "G5",
+                  "icon": "monument"
+              }
+          }, {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-121.328415, 44.056495]
+            },
+            "properties": {
+                "title": "Gavleston",
+                "icon": "monument"
+            }
+        }, {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-121.324075, 44.04412]
+            },
+            "properties": {
+                "title": "Graduate Research Center",
+                "icon": "monument"
+            }
+        }, {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-121.316145, 44.04351]
+            },
+            "properties": {
+                "title": "Old Mill",
+                "icon": "monument"
+            }
+        }, {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-121.333995, 44.04276]
+            },
+            "properties": {
+                "title": "OSU Cascades",
+                "icon": "monument"
+            }
+        }, {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-121.313305, 44.060185]
+            },
+            "properties": {
+                "title": "Ten Barrel",
+                "icon": "monument"
+            }
+        }]
+        }
+    },
+    "layout": {
+        "icon-image": "{icon}-15",
+        "text-field": "{title}",
+        "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+        "text-offset": [0, 0.6],
+        "text-anchor": "top"
+    }
+});
+
 }
